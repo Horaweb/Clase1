50b43e935924edca6a050c1755c17e9e1cdc9cd5
Horaweb <you@example.com>
2021-09-19 22:04:53 -0300

    Primer desafio

---
+++ Desafio 1/main.js
@@ -0,0 +1,8 @@
+var edad = prompt("Por favor, ingrese su edad")
+
+        var peso = prompt("Por favor, ingrese sus peso expresado en Kilogramos")
+
+        var resultado = parseInt (edad) + parseFloat (peso);
+
+        alert("La suma de su edad y peso es: " + resultado)
+        
